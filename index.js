ff3724494f423d2ac92ad53b365ab5f7de8a7531
Payton Kentch <frankrlystan@gmail.com>
2023-07-10 10:02:50 +0000

    im scripting this push, teehee

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log("Hello world!")
+const formBtn = document.querySelector("#submit-btn");
+formBtn.addEventListener("click", (event) => {
+  event.preventDefault();
+  const elmnt = document.createElement("li");
+  elmnt.innerText = document.getElementById("input1").value;
+  document.querySelector("#languages").appendChild(elmnt);
+});
